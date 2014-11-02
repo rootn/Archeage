930a1ade253f94739b560c054d27df6e0239fb85
rootn <rootapplepie@gmail.com>
2014-11-01 17:32:32 -0700

    House Plotter

---
+++ House Plotter/House Plotter/PlotForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Plotter.Source;
+
+namespace Plotter
+{
+    public partial class PlotForm : Form
+    {
+        public int housePos1;
+        public int housePos2;
+        public int housePos3;
+        private uint houseID;
+        housePlotter plotter;
+
+        public PlotForm()
+        {
+            InitializeComponent();
+        }
+
+        public void setPlotter(housePlotter obj)
+        {
+            plotter = obj;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plotter.markHouseOnMap(housePos1, housePos2, housePos3);
+        }
+
+        private void PlotForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            housePos1 = Int32.Parse(this.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            housePos2 = Int32.Parse(this.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            housePos3 = Int32.Parse(this.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0) houseID = 21168;
+            else if (comboBox1.SelectedIndex == 1) houseID = 21172;
+            else if (comboBox1.SelectedIndex == 2) houseID = 21171;
+            else if (comboBox1.SelectedIndex == 3) houseID = 15566;
+            else houseID = 21165;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            plotter.spamHouse(houseID, housePos1, housePos2, housePos3);
+        }
+    }
+}
---
+++ House Plotter/House Plotter/Plotter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using ArcheBuddy.Bot.Classes;
+
+namespace Plotter.Source
+{
+    public class housePlotter : Core
+    {
+        private Creature housePlot = null;
+        private PlotForm plotForm;
+        private Thread formThread;
+
+        public static string GetPluginAuthor()
+        {
+            return "RootN";
+        }
+
+        public static string GetPluginVersion()
+        {
+            return "1.0.0.0";
+        }
+
+        public static string GetPluginDescription()
+        {
+            return "House Plotter";
+        }
+
+        public void selectHouse(int pos1, int pos2)
+        {
+            foreach (var obj in getCreatures())
+            {
+                if (obj.X == pos1 && obj.Y == pos2)
+                {
+                    SetTarget(obj);
+                    housePlot = obj;
+                }
+
+            }
+        }
+
+        public void spamHouse(uint houseId, int pos1, int pos2, int pos3)
+        {
+            selectHouse(pos1, pos2);
+            Item design = getInvItem(houseId);
+            bool loopStart = true;
+            while (loopStart)
+            {
+                if (!isExists(housePlot))
+                {
+                    design.ConstructHouse(pos1, pos2, pos3, 0);
+                    loopStart = false;
+                }
+            }
+        }
+
+        public void markHouseOnMap(int pos1, int pos2, int pos3)
+        {
+            SetMapPos(pos1, pos2, pos3);
+        }
+
+        public void runForm()
+        {
+            try
+            {
+                Application.Run(plotForm);
+            }
+            catch (Exception error)
+            {
+                Log(error.ToString());
+            }
+        }
+
+        //Call on plugin start
+        public void PluginRun()
+        {
+            plotForm = new PlotForm();
+            plotForm.setPlotter(this);
+            formThread = new Thread(runForm);
+            formThread.Start();
+        }
+
+        //Call on plugin stop
+        public void PluginStop()
+        {
+        }
+    }
+}
